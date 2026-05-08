d938f4c5d2cfb0ecf2e4fec4c58774e7a28c4721
axios <phuongnguyen240805@gmail.com>
2026-05-08 16:24:44 +0700

    update project

--- libs/dto/src/account.dto.ts
+++ libs/dto/src/account.dto.ts
@@ -57,9 +57,13 @@ export class ResetPasswordDto {
   password?: string
 }
 
-export class MenuMeta extends PartialType(OmitType(MenuEntity, ['parentId', 'createdAt', 'updatedAt', 'id', 'roles', 'path', 'name'] as const)) {
-  title?: string
-}
+export class MenuMeta extends PartialType(
+  OmitType(MenuEntity, ['parentId', 'createdAt', 'updatedAt', 'id', 'roles', 'path', 'name'] as const)
+) {}
+
+// export class MenuMeta extends PartialType(OmitType(MenuEntity, ['parentId', 'createdAt', 'updatedAt', 'id', 'roles', 'path', 'name'] as const)) {
+//   title?: string
+// }
 export class AccountMenus extends PickType(MenuEntity, ['id', 'path', 'name', 'component'] as const) {
   meta?: MenuMeta
 }
